c91fb650ad03d44192a96f3726132ecca43142c8
sude <102838221+sude58@users.noreply.github.com>
2025-05-23 23:02:44 -0500

    Finished exercise 5

---
+++ basicoperations/5.py
@@ -0,0 +1,3 @@
+# Error would occur if you try to access a list element with an index greater ir equal to length since that index does not exist in the list. (Indexes are 0 to length - 1)
+foo = ["a", "b", "c"]
+print(foo[3])  # will this result in an error? Yes
