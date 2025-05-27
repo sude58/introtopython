ea52ca95551979874d75d470b2dbe21ca137df79
sude <102838221+sude58@users.noreply.github.com>
2025-05-26 19:42:07 -0500

    Finished Exercise 1

---
+++ variables/1.py
@@ -0,0 +1,11 @@
+# Variable name convention
+"""
+index: idiomatic
+CatName: non-idiomatic, following Pascal case instead of snake case
+lazy_dog: idiomatic
+quick_Fox: non-idiomatic, there should be no capitalized character
+1stCharacter: illegal, name cannot start with digit
+operand2: idiomatic
+BIG_NUMBER: non-idiomatic, using screaming snake case instead of snake case
+π: non-idiomatic, using non-ASCII character
+"""
