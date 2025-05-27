23aa028f2bd57ba8158ce1ebe772e9116e5d3ba1
sude <102838221+sude58@users.noreply.github.com>
2025-05-26 19:48:50 -0500

    Finished Exercise 3

---
+++ variables/3.py
@@ -0,0 +1,11 @@
+# Constant naming convention
+"""
+index: non-idiomatic, using snake case instead of screaming snake case
+CatName: non-idiomatic, using pascal case instead of screaming snake case
+snake_case: non-idiomatic, using snake case instead of screaming snake case
+LAZY_DOG3: idiomatic
+1ST: illegal, starting with digits
+operand2, non-idiomatic, using snake case instead of screaming snake case
+BIG_NUMBER: idiomatic
+Π: non-idiomatic, using non-standard ASCII character
+"""
